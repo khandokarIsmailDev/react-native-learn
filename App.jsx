a85794a2b168f4b79ed094d931d8249bef3c6401
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-11-17 06:43:12 +0600

    first run my manual code

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text style={{fontSize: 30}}>Hello Ismail</Text>
+      <Text style={{fontSize:50}}>LIke</Text>
+    </View>
+  );
+}
+
