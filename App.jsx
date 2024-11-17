a273b5348353acfab7f61469346d4e1dd783d159
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-11-17 16:20:36 +0600

    Grid-using FlatList

--- App.jsx
+++ App.jsx
@@ -15,85 +15,94 @@ import {
 } from 'react-native';
 
 export default function App() {
-
-  
-  const users =[
+  const users = [
     {
-      id:1,
-      name:"Sam"
+      id: 1,
+      name: 'Sam',
     },
     {
-      id:2,
-      name:"Kam"
+      id: 2,
+      name: 'Kam',
     },
     {
-      id:3,
-      name:"Pem"
+      id: 3,
+      name: 'Pem',
     },
     {
-      id:4,
-      name:"Cham"
+      id: 4,
+      name: 'Cham',
     },
     {
-      id:5,
-      name:"Duam"
+      id: 5,
+      name: 'Duam',
     },
     {
-      id:5,
-      name:"Duam"
+      id: 6,
+      name: 'Duam',
     },
     {
-      id:5,
-      name:"Duam"
+      id: 7,
+      name: 'Duam',
     },
     {
-      id:5,
-      name:"Duam"
+      id: 8,
+      name: 'Duam',
     },
     {
-      id:5,
-      name:"Duam"
+      id: 15,
+      name: 'Duam',
     },
     {
-      id:5,
-      name:"Duam"
+      id: 25,
+      name: 'Duam',
     },
     {
-      id:5,
-      name:"Duam"
-    }
-  ]
-
-
- 
-
+      id: 35,
+      name: 'Duam',
+    },
+  ];
 
   return (
     <View>
-      <Text style={{fontSize:19,fontWeight:'bold',textAlign:'center',backgroundColor:'blue',color:'#fff'}} >Using Grid List In Map</Text>
-      <View style={{flex:1,flexDirection:'row',flexWrap:'wrap'}}>
-        {
-          users.map((item) => <Text style={customStyles.item}>{item.name}</Text>)
-        }
-        
-        
+      <Text
+        style={{
+          fontSize: 19,
+          fontWeight: 'bold',
+          textAlign: 'center',
+          backgroundColor: 'blue',
+          color: '#fff',
+        }}>
+        Using Grid List In Map
+      </Text>
+      <View style={{flex: 1, flexDirection: 'row', flexWrap: 'wrap'}}>
+        <FlatList
+          data={users}
+          renderItem={({item}) => (
+            <View style={customStyles.box}>
+              <Text style={customStyles.item}>{item.id}</Text>
+              <Text style={customStyles.item}>{item.name}</Text>
+            </View>
+          )}
+        />
       </View>
     </View>
   );
 }
 
 const customStyles = StyleSheet.create({
-  item:{
-    fontSize:19,
-    fontWeight:'bold',
-    backgroundColor:'red',
-    color:'white',
-    padding:5,
-    borderRadius:10,
-    width:120,
-    height:120,
-    textAlignVertical:'center',
-    textAlign:'center',
-    margin:5
-  }
-})
+  item: {
+    fontSize: 24,
+    color: 'orange',
+    fontWeight: 'bold',
+    flex: 1,
+    margin: 2,
+    paddingLeft:10
+  },
+  box: {
+    flexDirection: 'row',
+    borderWidth: 2,
+    borderColor: 'orange',
+    marginBottom: 10,
+
+  },
+});
