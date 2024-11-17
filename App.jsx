a1cf3aa7c39feeeded2f8ee55bb8df7b40a1db3d
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-11-17 13:10:17 +0600

    how to map an list array; but map doest not support here;

--- App.jsx
+++ App.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
-import EXstyles from './style'
+import React, {useState} from 'react';
+import EXstyles from './style';
 
 import {
   Button,
+  FlatList,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -15,88 +16,38 @@ import {
 
 export default function App() {
 
-  const[name,setName] = useState('')
-  const [email,setEmail] = useState('')
-  const [password,setPassword] = useState('')
-  const[display,setDisplay] = useState(false)
-
-  const resetFormData =() =>{
-    setDisplay(false)
-    setName("")
-    setEmail("")
-    setPassword("")
-  }
-
+ 
+  const users =[
+    {
+      id:1,
+      name:"Sam"
+    },
+    {
+      id:2,
+      name:"Kam"
+    },
+    {
+      id:3,
+      name:"Pem"
+    },
+    {
+      id:4,
+      name:"Cham"
+    },
+    {
+      id:5,
+      name:"Duam"
+    }
+  ]
 
   return (
-    <>
-      
-      
-
-      <Text style={{fontSize:30}} >Handle Text Input</Text>
-      <Text style={customStyle.textTitle} >Name is : <Text style={customStyle.textSpan}>{name}</Text></Text>
-      <Text style={customStyle.textTitle} >Email is : <Text style={customStyle.textSpan}>{email}</Text></Text>
-
-      <TextInput 
-        style={customStyle.textArea}
-        placeholder='Enter Your Name'
-        onChangeText={(text) =>setName(text)}
-        value={name}
+    <View>
+      <Text style={{fontSize:19,fontWeight:'bold'}} >All Users List</Text>
+      <FlatList
+        data={users}
+        renderItem={({item}) =><Text style={{fontSize:20}}>{item.name}</Text>}
+        keyExtractor={item=>item.id}
       />
-
-      <TextInput
-      style={customStyle.textArea}
-      placeholder='Enter Your Email'
-      onChangeText={(email) => setEmail(email)}
-      value={email}
-      />
-
-      <TextInput
-      style={customStyle.textArea}
-      placeholder='Enter Your Password'
-      onChangeText={(pass) => setPassword(pass)}
-      keyboardType='password'
-      value={password}
-      secureTextEntry={true}
-      />
-
-      {/* Button e sorasori style likha jay na  */}
-      <View style={{marginBottom:10}}>
-        <Button 
-        color={'green'} 
-        title='Print Detail'
-        onPress={()=>setDisplay(true)}
-        />
-      </View>
-      
-      <Button title='clear Detail' onPress={resetFormData}/>
-
-      {
-        display && (
-          <View>
-            <Text style={{fontSize:19,fontWeight:'bold'}}> User Name is : {name}</Text>
-            <Text style={{fontSize:19,fontWeight:'bold'}}> User Email is : {email}</Text>
-            <Text style={{fontSize:19,fontWeight:'bold'}}> User Password is : {password}</Text>
-          </View>
-        )
-      }
-      
-
-    </>
+    </View>
   );
 }
-
-const customStyle ={
-  textArea:{
-    fontSize:20,color:'black',borderWidth:2,borderColor:'blue',borderRadius:10,paddingTop:10,marginBottom:10,
-  },
-  textTitle:{
-    fontSize:18,
-    fontWeight:'bold'
-  },
-  textSpan:{
-    color:'red'
-  }
-}
-
-
